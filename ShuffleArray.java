5013345ff97a7a21cc3bbbbcc4aab72925b6b5c1
Vinodkumar199924 <147994592+Vinodkumar199924@users.noreply.github.com>
2023-10-17 15:46:13 +0530

    added shuffleArray.java

---
+++ ShuffleArray.java
@@ -0,0 +1,29 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ShuffleArray {
+
+public static void shufflearray(int[] arr) {
+	Random rnum = new Random();
+	
+	for (int i=0; i<arr.length; i++) {
+	    int randomPos= rnum.nextInt(arr.length);
+	    int temp = arr[i];
+	    arr[i] = arr[randomPos];
+	    arr[randomPos] = temp;
+	}
+
+	System.out.println(Arrays.toString(arr));
+}
+public static void main(String[] args) {
+
+	int [] a= {1,2,3,4,5};
+	System.out.println(Arrays.toString(a));
+	shufflearray(a);
+}
+}
